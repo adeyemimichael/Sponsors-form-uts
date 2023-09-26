f09580b3e38987b440166a10da19cd7e9350caab
adeyemimike123 <ayobami732000@gmail.com>
2023-09-26 11:50:18 +0100

    worked on the form database system

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 'use client';
-
+import { useState } from 'react';
 import { Button,  Label, TextInput } from 'flowbite-react';
-
+import axios from "axios";
 export default function ShadowInputs() {
+
+
+  const [email, SetEmail] = useState(" ");
+  const [Fullname , SetUsername] = useState(" ");
+  const [organization, SetOrganization] = useState(" ");
+  const [Phonenumber, SetPhonenumber] = useState(" ");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(username, email);
+    const Data = {
+      email: email,
+      name:Fullname ,
+      organization:organization,
+      Phonenumber:Phonenumber
+    };
+    axios.post(
+        "https://sheet.best/api/sheets/9cf3c6f0-2562-455f-aa59-fe1b5708b06f",
+        Data
+      )
+      .then((response) => {
+        console.log(response);
+        SetEmail("");
+        SetUsername("");
+        SetOrganization("");
+        SetPhonenumber("");
+      });
+    }
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-[#000038]'>
       <h1 className='font-bold text-[30px] text-white'>Want to Sponsor Unilorin tech Summit? </h1>
       <p className='text-gray-100 font-normal text-[18px]'>Fill the form below and one of our representative will reach out to in the next 24 hours </p>
-    <form className="flex max-w-md flex-col gap-4 bg-[#b5fc1d] p-10 md:w-[50%]  w-[100%] md:h-[80%]">
+    <form className="flex max-w-md flex-col gap-4 bg-[#b5fc1d] p-10 md:w-[50%]  w-[100%] md:h-[80%]" onSubmit={handleSubmit}>
       <div>
         <div className="mb-2 block">
           <Label
@@ -18,23 +46,29 @@ export default function ShadowInputs() {
         <TextInput
           id="email2"
           placeholder="name@mail.com"
-          required
+          // required
           shadow
+          onChange={(e) => SetEmail(e.target.value)}
+          value={email}
           type="email"
+       
         />
       </div>
       <div>
         <div className="mb-2 block">
           <Label
             htmlFor="fullname "
-            value="Full name "
+            value="Fullname "
           />
         </div>
         <TextInput
-          id="name "
-          required
+          id="name"
+          // required
           shadow
+          onChange={(e) => SetUsername(e.target.value)}
+          value={Fullname }
           type="text"
+        
         />
       </div>
       <div>
@@ -46,9 +80,12 @@ export default function ShadowInputs() {
         </div>
         <TextInput
           id="organization name "
-          required
+          // required
           shadow
+          onChange={(e) => SetOrganization(e.target.value)}
+          value={organization}
           type="text"
+       
         />
       </div>
       <div className="flex items-center gap-2">
@@ -61,10 +98,12 @@ export default function ShadowInputs() {
         </Label>
         <TextInput
           id="phone number  "
-          required
+          // required
           shadow
-          type="number"
-         
+          type="number "
+          onChange={(e) => SetPhonenumber(e.target.value)}
+            value={Phonenumber}
+          
          className='w-[100%]'
         />
         </div>
